e71316def00e3f388be27f06be7c9aafe41b8d78
mraurathegoat <920803@lcps.org>
2026-01-22 00:12:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function playGame() {
+  const goalsFor = Math.floor(Math.random()*6);
+  const goalsAgainst = Math.floor(Math.random()*6);
 
+  let resultText=`You lost ${goalsFor}-${goalsAgainst}`;
+
+  if (goalsFor>goalsAgainst){
+      resultText=`You won ${goalsFor}-${goalsAgainst}!`;
+  } else if (goalsFor === goalsAgainst) {
+    resultText = `You tied ${goalsFor}-${goalsAgainst};
+    }
+
+  document.getElementById("result").textContent = resultText;
+}
